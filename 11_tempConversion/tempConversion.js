bbb5b47eaf3885a4e2930806fb5a00bfb68f80b2
zachariah-spencer <spencerzachariah@gmail.com>
2025-10-09 10:36:04 -0500

    Complete 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  result = (temp - 32) * (5/9);
+  roundedResult = Math.round(result * 10) / 10;
+  return roundedResult;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  result = (temp * (9/5)) + 32;
+  roundedResult = Math.round(result * 10) / 10;
+  return roundedResult;
 };
 
 // Do not edit below this line
