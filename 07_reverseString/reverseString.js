34210e62a4cf8d277d7ce986124b15bee0a52aa5
zachariah-spencer <spencerzachariah@gmail.com>
2025-10-08 22:54:46 -0500

    Complete 07

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(string) {
+  reversed = "";
+  for(let i = 1; i <= string.length; i++){
+    reversed += string[string.length - i];
+  }
 
+  return reversed;
 };
 
 // Do not edit below this line
