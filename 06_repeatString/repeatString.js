1dba8ec6bedd1186e5bdda1a3379bbf798e07d17
zachariah-spencer <spencerzachariah@gmail.com>
2025-10-08 22:48:21 -0500

    Complete 06

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+  newString = "";
 
+  if(num >= 0){
+    for(let i = 0; i < num; i++){
+      newString += string;
+    }
+    return newString;
+  } else {
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line
