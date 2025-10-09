4c328872e919e926b5be2626d2f9b5e82d8fb1f9
zachariah-spencer <spencerzachariah@gmail.com>
2025-10-08 23:23:59 -0500

    Complete 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+  if(typeof(a) != "number" || typeof(b) != "number"){
+    return "ERROR";
+  }
 
+  if(a < 0 || b < 0){
+    return "ERROR";
+  }
+
+  if(a % 1 !== 0 || b % 1 !== 0){
+    return "ERROR";
+  }
+
+  let i = (a < b) ? a : b;
+  let end = (a > b) ? a : b;
+  let sum = 0;
+
+  for(; i <= end; i++){
+    sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
